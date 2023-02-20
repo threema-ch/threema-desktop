65dca083536ddad146e37e19fa81ffac10bdde56
Danilo Bargen <danilo.bargen@threema.ch>
2023-02-20 22:40:21 +0100

    Apply blob download logic to both in- and outbound messages

--- src/common/model/message/file-message.ts
+++ src/common/model/message/file-message.ts
@@ -5,6 +5,7 @@ import {
     type DbCreate,
     type DbFileMessage,
     type DbMessageCommon,
+    type DbMessageUid,
 } from '~/common/db';
 import {BlobDownloadState, MessageDirection, MessageType, ReceiverType} from '~/common/enum';
 import {deleteFilesInBackground} from '~/common/file-storage';
@@ -30,6 +31,7 @@ import {
     type ServicesForModel,
     type UidOf,
 } from '~/common/model';
+import {type ModelLifetimeGuard} from '~/common/model/utils/model-lifetime-guard';
 import {LocalModelStore} from '~/common/model/utils/model-store';
 import {BlobBackendError, type BlobScope} from '~/common/network/protocol/blob';
 import {BLOB_FILE_NONCE, BLOB_THUMBNAIL_NONCE} from '~/common/network/protocol/constants';
@@ -170,6 +172,204 @@ function getPreviewText({
     return (caption ?? fileName)?.slice(0, PREVIEW_MESSAGE_MAX_TEXT_LENGTH);
 }
 
+async function downloadBlob(
+    type: 'main',
+    messageUid: DbMessageUid,
+    conversation: ConversationControllerHandle,
+    services: ServicesForModel,
+    lock: AsyncLock,
+    meta: ModelLifetimeGuard<InboundFileMessageView> | ModelLifetimeGuard<OutboundFileMessageView>,
+    log: Logger,
+): Promise<ReadonlyUint8Array>;
+async function downloadBlob(
+    type: 'thumbnail',
+    messageUid: DbMessageUid,
+    conversation: ConversationControllerHandle,
+    services: ServicesForModel,
+    lock: AsyncLock,
+    meta: ModelLifetimeGuard<InboundFileMessageView> | ModelLifetimeGuard<OutboundFileMessageView>,
+    log: Logger,
+): Promise<ReadonlyUint8Array | undefined>;
+/**
+ * Download blob of the specified type.
+ *
+ * If the blob has not yet been downloaded, the download will be started and the database will be
+ * updated. Once that is done, the promise will resolve with the blob data.
+ */
+async function downloadBlob(
+    type: 'main' | 'thumbnail',
+    messageUid: DbMessageUid,
+    conversation: ConversationControllerHandle,
+    services: ServicesForModel,
+    lock: AsyncLock,
+    meta: ModelLifetimeGuard<InboundFileMessageView> | ModelLifetimeGuard<OutboundFileMessageView>,
+    log: Logger,
+): Promise<ReadonlyUint8Array | undefined> {
+    const {blob, crypto, file} = services;
+
+    function markDownloadAsFailed(): void {
+        let change: Partial<FileMessageViewFragment>;
+        switch (type) {
+            case 'main':
+                change = {blobDownloadState: BlobDownloadState.FAILED};
+                break;
+            case 'thumbnail':
+                change = {thumbnailBlobDownloadState: BlobDownloadState.FAILED};
+                break;
+            default:
+                unreachable(type);
+        }
+        meta.update(() => {
+            updateFileMessage(services, log, conversation.uid, messageUid, change);
+            return {change, state: 'failed'};
+        });
+    }
+
+    // Because the download logic is async and consists of multiple steps, we need a lock to
+    // avoid races where the same blob is downloaded multiple times.
+    return await lock.with(async () => {
+        // If blob is already downloaded (i.e. a fileId is set), return it
+        const existingFileData: FileData | undefined = meta.run((handle) => {
+            switch (type) {
+                case 'main':
+                    return handle.view().fileData;
+                case 'thumbnail':
+                    return handle.view().thumbnailFileData;
+                default:
+                    return unreachable(type);
+            }
+        });
+        if (existingFileData !== undefined) {
+            return await file.load(existingFileData);
+        }
+
+        // If blob is marked as permanently failed, don't attempt to download
+        const downloadState = meta.run((handle) => {
+            switch (type) {
+                case 'main':
+                    return handle.view().blobDownloadState;
+                case 'thumbnail':
+                    return handle.view().thumbnailBlobDownloadState;
+                default:
+                    return unreachable(type);
+            }
+        });
+        if (downloadState === BlobDownloadState.FAILED) {
+            switch (type) {
+                case 'main':
+                    throw new Error('Blob download is marked as failed');
+                case 'thumbnail':
+                    return undefined;
+                default:
+                    return unreachable(type);
+            }
+        }
+
+        // Get matching blob ID and nonce
+        const [blobId, nonce] = meta.run((handle) => {
+            switch (type) {
+                case 'main':
+                    return [handle.view().blobId, BLOB_FILE_NONCE];
+                case 'thumbnail':
+                    return [handle.view().thumbnailBlobId, BLOB_THUMBNAIL_NONCE];
+                default:
+                    return unreachable(type);
+            }
+        });
+
+        // If there is no blob ID and no file data, there's nothing to be downloaded
+        if (blobId === undefined) {
+            markDownloadAsFailed();
+            throw new Error('Both file data and blob ID are missing');
+        }
+
+        // Otherwise, download blob from the blob mirror
+        log.debug(`Downloading ${type} blob`);
+        if (type === 'main') {
+            meta.update((view) => ({state: 'syncing'}));
+        }
+        let downloadResult;
+        try {
+            downloadResult = await blob.download('public', blobId);
+        } catch (error) {
+            if (error instanceof BlobBackendError && error.type === 'not-found') {
+                // Irrecoverable error, blob not found.
+                markDownloadAsFailed();
+            }
+            throw ensureError(error);
+        }
+
+        // Decrypt bytes
+        const secretBox = crypto.getSecretBox(
+            meta.run((handle) => handle.view().encryptionKey),
+            NONCE_UNGUARDED_TOKEN,
+        );
+        const decryptedBytes = secretBox
+            .decryptorWithNonce(CREATE_BUFFER_TOKEN, nonce, downloadResult.data)
+            .decrypt();
+
+        // Blob downloaded, store in file storage
+        const storedFile = await file.store(decryptedBytes);
+        const storedFileData = {
+            fileId: storedFile.fileId,
+            encryptionKey: storedFile.encryptionKey,
+            unencryptedByteCount: decryptedBytes.byteLength,
+            storageFormatVersion: storedFile.storageFormatVersion,
+        };
+
+        // Update database
+        let change: Partial<FileMessageViewFragment>;
+        switch (type) {
+            case 'main':
+                change = {fileData: storedFileData};
+                break;
+            case 'thumbnail':
+                change = {thumbnailFileData: storedFileData};
+                break;
+            default:
+                unreachable(type);
+        }
+        meta.update(() => {
+            updateFileMessage(services, log, conversation.uid, messageUid, change);
+            switch (type) {
+                case 'main':
+                    return {change, state: 'synced'};
+                case 'thumbnail':
+                    return {change};
+                default:
+                    return unreachable(type);
+            }
+        });
+
+        // Mark as downloaded
+        //
+        // Note: This is done in the background, we don't await the result of the done call.
+        let blobDoneScope: BlobScope;
+        switch (conversation.receiverLookup.type) {
+            case ReceiverType.CONTACT:
+            case ReceiverType.DISTRIBUTION_LIST:
+                // For contacts and distribution lists, use public scope,
+                // so that the blobs are removed from the public blob server.
+                blobDoneScope = 'public';
+                break;
+            case ReceiverType.GROUP:
+                // For groups, use local scope, so that the blobs are retained
+                // for the other group members.
+                blobDoneScope = 'local';
+                break;
+            default:
+                unreachable(conversation.receiverLookup);
+        }
+        downloadResult.done(blobDoneScope).catch((e) => {
+            log.error(`Failed to mark blob with id ${bytesToHex(blobId)} as done`, e);
+        });
+        log.debug(`Downloaded ${type} blob`);
+
+        // Return data
+        return decryptedBytes;
+    });
+}
+
 export class InboundFileMessageModelController
     extends InboundBaseMessageModelController<InboundFileMessage['view']>
     implements InboundFileMessageController
@@ -177,195 +377,32 @@ export class InboundFileMessageModelController
     /**
      * Async lock to guard blob download logic.
      */
-    private readonly _lock = new AsyncLock();
+    protected readonly _lock = new AsyncLock();
 
     /** @inheritdoc */
     public async blob(): Promise<ReadonlyUint8Array> {
-        return await this._blob('main');
+        return await downloadBlob(
+            'main',
+            this._uid,
+            this._conversation,
+            this._services,
+            this._lock,
+            this.meta,
+            this._log,
+        );
     }
 
     /** @inheritdoc */
     public async thumbnailBlob(): Promise<ReadonlyUint8Array | undefined> {
-        return await this._blob('thumbnail');
-    }
-
-    protected async _blob(type: 'main'): Promise<ReadonlyUint8Array>;
-    protected async _blob(type: 'thumbnail'): Promise<ReadonlyUint8Array | undefined>;
-    /**
-     * Download blob of the specified type.
-     *
-     * If the blob has not yet been downloaded, the download will be started and the database will
-     * be updated. Once that is done, the promise will resolve with the blob data.
-     */
-    protected async _blob(type: 'main' | 'thumbnail'): Promise<ReadonlyUint8Array | undefined> {
-        const {blob, crypto, file} = this._services;
-
-        // Because the download logic is async and consists of multiple steps, we need a lock to
-        // avoid races where the same blob is downloaded multiple times.
-        return await this._lock.with(async () => {
-            // If blob is already downloaded (i.e. a fileId is set), return it
-            const existingFileData: FileData | undefined = this.meta.run((handle) => {
-                switch (type) {
-                    case 'main':
-                        return handle.view().fileData;
-                    case 'thumbnail':
-                        return handle.view().thumbnailFileData;
-                    default:
-                        return unreachable(type);
-                }
-            });
-            if (existingFileData !== undefined) {
-                return await file.load(existingFileData);
-            }
-
-            // If blob is marked as failed, don't attempt to download
-            const downloadState = this.meta.run((handle) => {
-                switch (type) {
-                    case 'main':
-                        return handle.view().blobDownloadState;
-                    case 'thumbnail':
-                        return handle.view().thumbnailBlobDownloadState;
-                    default:
-                        return unreachable(type);
-                }
-            });
-            if (downloadState === BlobDownloadState.FAILED) {
-                switch (type) {
-                    case 'main':
-                        throw new Error('Blob download is marked as failed');
-                    case 'thumbnail':
-                        return undefined;
-                    default:
-                        return unreachable(type);
-                }
-            }
-
-            // If there is no blob ID, there's nothing to be downloaded
-            const [blobId, nonce] = this.meta.run((handle) => {
-                switch (type) {
-                    case 'main':
-                        return [handle.view().blobId, BLOB_FILE_NONCE];
-                    case 'thumbnail':
-                        return [handle.view().thumbnailBlobId, BLOB_THUMBNAIL_NONCE];
-                    default:
-                        return unreachable(type);
-                }
-            });
-            if (blobId === undefined) {
-                return undefined;
-            }
-
-            // Otherwise, download it from the blob mirror
-            this._log.debug(`Downloading ${type} blob`);
-            if (type === 'main') {
-                this.meta.update((view) => ({state: 'syncing'}));
-            }
-            let downloadResult;
-            try {
-                downloadResult = await blob.download('public', blobId);
-            } catch (error) {
-                if (error instanceof BlobBackendError && error.type === 'not-found') {
-                    // Irrecoverable error, blob not found.
-                    let change: Partial<InboundFileMessageView>;
-                    switch (type) {
-                        case 'main':
-                            change = {blobDownloadState: BlobDownloadState.FAILED};
-                            break;
-                        case 'thumbnail':
-                            change = {thumbnailBlobDownloadState: BlobDownloadState.FAILED};
-                            break;
-                        default:
-                            unreachable(type);
-                    }
-                    this.meta.update(() => {
-                        updateFileMessage(
-                            this._services,
-                            this._log,
-                            this._conversation.uid,
-                            this._uid,
-                            change,
-                        );
-                        return {change, state: 'failed'};
-                    });
-                }
-                throw ensureError(error);
-            }
-
-            // Decrypt bytes
-            const secretBox = crypto.getSecretBox(
-                this.meta.run((handle) => handle.view().encryptionKey),
-                NONCE_UNGUARDED_TOKEN,
-            );
-            const decryptedBytes = secretBox
-                .decryptorWithNonce(CREATE_BUFFER_TOKEN, nonce, downloadResult.data)
-                .decrypt();
-
-            // Blob downloaded, store in file storage
-            const storedFile = await file.store(decryptedBytes);
-            const storedFileData = {
-                fileId: storedFile.fileId,
-                encryptionKey: storedFile.encryptionKey,
-                unencryptedByteCount: decryptedBytes.byteLength,
-                storageFormatVersion: storedFile.storageFormatVersion,
-            };
-
-            // Update database
-            let change: Partial<InboundFileMessageView>;
-            switch (type) {
-                case 'main':
-                    change = {fileData: storedFileData};
-                    break;
-                case 'thumbnail':
-                    change = {thumbnailFileData: storedFileData};
-                    break;
-                default:
-                    unreachable(type);
-            }
-            this.meta.update(() => {
-                updateFileMessage(
-                    this._services,
-                    this._log,
-                    this._conversation.uid,
-                    this._uid,
-                    change,
-                );
-                switch (type) {
-                    case 'main':
-                        return {change, state: 'synced'};
-                    case 'thumbnail':
-                        return {change};
-                    default:
-                        return unreachable(type);
-                }
-            });
-
-            // Mark as downloaded
-            //
-            // Note: This is done in the background, we don't await the result of the done call.
-            let blobDoneScope: BlobScope;
-            switch (this._conversation.receiverLookup.type) {
-                case ReceiverType.CONTACT:
-                case ReceiverType.DISTRIBUTION_LIST:
-                    // For contacts and distribution lists, use public scope,
-                    // so that the blobs are removed from the public blob server.
-                    blobDoneScope = 'public';
-                    break;
-                case ReceiverType.GROUP:
-                    // For groups, use local scope, so that the blobs are retained
-                    // for the other group members.
-                    blobDoneScope = 'local';
-                    break;
-                default:
-                    unreachable(this._conversation.receiverLookup);
-            }
-            downloadResult.done(blobDoneScope).catch((e) => {
-                this._log.error(`Failed to mark blob with id ${bytesToHex(blobId)} as done`, e);
-            });
-            this._log.debug(`Downloaded ${type} blob`);
-
-            // Return data
-            return decryptedBytes;
-        });
+        return await downloadBlob(
+            'thumbnail',
+            this._uid,
+            this._conversation,
+            this._services,
+            this._lock,
+            this.meta,
+            this._log,
+        );
     }
 
     /** @inheritdoc */
@@ -378,21 +415,35 @@ export class OutboundFileMessageModelController
     extends OutboundBaseMessageModelController<OutboundFileMessage['view']>
     implements OutboundFileMessageController
 {
+    /**
+     * Async lock to guard blob download logic.
+     */
+    protected readonly _lock = new AsyncLock();
+
     /** @inheritdoc */
     public async blob(): Promise<ReadonlyUint8Array> {
-        const fileData = this.meta.run((handle) => handle.view().fileData);
-        assert(fileData !== undefined, 'File data for outgoing message is undefined');
-        return await this._services.file.load(fileData);
-        // TODO(DESK-307): For reflected messages, it's possible that the blob is not yet downloaded
+        return await downloadBlob(
+            'main',
+            this._uid,
+            this._conversation,
+            this._services,
+            this._lock,
+            this.meta,
+            this._log,
+        );
     }
 
     /** @inheritdoc */
     public async thumbnailBlob(): Promise<ReadonlyUint8Array | undefined> {
-        const thumbnailFileData = this.meta.run((handle) => handle.view().thumbnailFileData);
-        return thumbnailFileData === undefined
-            ? undefined
-            : await this._services.file.load(thumbnailFileData);
-        // TODO(DESK-307): For reflected messages, it's possible that the blob is not yet downloaded
+        return await downloadBlob(
+            'thumbnail',
+            this._uid,
+            this._conversation,
+            this._services,
+            this._lock,
+            this.meta,
+            this._log,
+        );
     }
 
     /** @inheritdoc */
