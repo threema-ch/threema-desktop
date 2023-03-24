7923186549c513761a4d9bd77293ff69a5872e50
Danilo Bargen <danilo.bargen@threema.ch>
2023-03-24 18:29:23 +0100

    Disable incompatible unit tests on Windows
    After this change, all remaining Mocha unit tests should pass on
    Windows.

--- src/test/mocha/common/file-storage.spec.ts
+++ src/test/mocha/common/file-storage.spec.ts
@@ -206,20 +206,26 @@ export function run(): void {
             genericStorageTests(() => fileStorage);
 
             describe('store', function () {
-                // Note: Unfortunately this test does not work in GitLab CI, due to the way
-                // permissions are set up there. On a normal development machine, it should work.
-                if (env.GITLAB_CI === undefined) {
-                    it('propagates the I/O error if the storage directory cannot be written', async function () {
-                        fs.chmodSync(storageDirPath, 0o000); // Make it readonly
-                        await assertFileStorageError(
-                            fileStorage.store(Uint8Array.of(1, 2, 3, 4)),
-                            'write-error',
-                            undefined,
-                            'EACCES',
-                            'permission denied',
-                        );
-                    });
-                }
+                it('propagates the I/O error if the storage directory cannot be written', async function () {
+                    if (env.GITLAB_CI !== undefined) {
+                        // Unfortunately this test does not work in GitLab CI, due to the way
+                        // permissions are set up there. On a normal development machine, it should
+                        // work.
+                        this.skip();
+                    }
+                    if (process.platform === 'win32') {
+                        // File permissions are not fully supported on Windows, thus this test won't work.
+                        this.skip();
+                    }
+                    fs.chmodSync(storageDirPath, 0o000); // Make it readonly
+                    await assertFileStorageError(
+                        fileStorage.store(Uint8Array.of(1, 2, 3, 4)),
+                        'write-error',
+                        undefined,
+                        'EACCES',
+                        'permission denied',
+                    );
+                });
 
                 it('will error if file already exists', async function () {
                     // To test this, we'll use a RNG that always returns the same bytes.
@@ -256,6 +262,10 @@ export function run(): void {
                 });
 
                 it('ensure proper file mode', async function () {
+                    if (process.platform === 'win32') {
+                        // File modes not supported under Windows
+                        this.skip();
+                    }
                     const {fileId} = await fileStorage.store(Uint8Array.of(1, 2, 3, 4));
                     const stat = fs.statSync(path.join(storageDirPath, fileId.slice(0, 2), fileId));
                     // eslint-disable-next-line no-bitwise
@@ -330,21 +340,27 @@ export function run(): void {
                     expect(await fileStorage.delete(randomFileId(crypto))).to.be.false;
                 });
 
-                // Note: Unfortunately this test does not work in GitLab CI, due to the way
-                // permissions are set up there. On a normal development machine, it should work.
-                if (env.GITLAB_CI === undefined) {
-                    it('propagates the I/O error if the file is readonly', async function () {
-                        const fileId = randomFileId(crypto);
-                        fs.chmodSync(storageDirPath, 0o000); // Make it readonly
-                        await assertFileStorageError(
-                            fileStorage.delete(fileId),
-                            'delete-error',
-                            'Could not delete file with ID',
-                            'EACCES',
-                            'permission denied',
-                        );
-                    });
-                }
+                it('propagates the I/O error if the file is readonly', async function () {
+                    if (env.GITLAB_CI !== undefined) {
+                        // Unfortunately this test does not work in GitLab CI, due to the way
+                        // permissions are set up there. On a normal development machine, it should
+                        // work.
+                        this.skip();
+                    }
+                    if (process.platform === 'win32') {
+                        // File permissions are not fully supported on Windows, thus this test won't work.
+                        this.skip();
+                    }
+                    const fileId = randomFileId(crypto);
+                    fs.chmodSync(storageDirPath, 0o000); // Make it readonly
+                    await assertFileStorageError(
+                        fileStorage.delete(fileId),
+                        'delete-error',
+                        'Could not delete file with ID',
+                        'EACCES',
+                        'permission denied',
+                    );
+                });
             });
         });
     });
