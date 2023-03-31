3b7cbd3b23381b2e0b338a9353d168c1b25a3854
Danilo Bargen <danilo.bargen@threema.ch>
2023-03-31 09:37:00 +0200

    Dev container env script improvements
    - Add shebang line
    - Quoting
    - Proper argument assignment: https://www.shellcheck.net/wiki/SC2124
    - Consistent indentation

--- .devcontainer/env.sh
+++ .devcontainer/env.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Extract necessary vars
 NODE_VERSION=$(jq -e -r '.build.args.NODE_VERSION' ./.devcontainer/devcontainer.json)
 WORK_DIRECTORY=$(jq -e -r '.workspaceFolder' ./.devcontainer/devcontainer.json)
@@ -5,29 +7,32 @@ RUN_ARGS=$(jq -e -r '.runArgs | join(" ") | sub("\\${localEnv:(?<v>[^}]+)}"; env
 MOUNTS=$(jq -e -r '.mounts | map("--mount=\(sub("\\${localEnv:(?<v>[^}]+)}"; env[.v]))") | join(" ")' ./.devcontainer/devcontainer.json)
 
 # Build the Docker container for the current version, if necessary
-if [[ "$(docker images -q threema-desktop-env:${NODE_VERSION} 2> /dev/null)" == "" ]]; then
-  docker build \
-    --no-cache \
-    --build-arg NODE_VERSION=${NODE_VERSION} \
-    ./.devcontainer \
-    -t threema-desktop-env:${NODE_VERSION}
+DOCKER_IMAGE=$(docker images -q "threema-desktop-env:${NODE_VERSION}" 2> /dev/null)
+if [[ "$DOCKER_IMAGE" == "" ]]; then
+    docker build \
+      --no-cache \
+      --build-arg NODE_VERSION="${NODE_VERSION}" \
+      ./.devcontainer \
+      -t "threema-desktop-env:${NODE_VERSION}"
 fi
 
-# Alias `node` and `npm`
+# Run a command in the container
 function run_in_container {
-  args="$@"
-  bash -ic "
-  docker run \
-    -it \
-    ${RUN_ARGS} \
-    ${MOUNTS} \
-    --user=$(id -u):$(id -g) \
-    --workdir=${WORK_DIRECTORY} \
-    --volume=${PWD}:${WORK_DIRECTORY}:rw \
-    threema-desktop-env:${NODE_VERSION} \
-    /bin/bash -ic \"${args}\"
-  "
+    args="$*"
+    bash -ic "
+    docker run \
+      -it \
+      ${RUN_ARGS} \
+      ${MOUNTS} \
+      --user=$(id -u):$(id -g) \
+      --workdir=${WORK_DIRECTORY} \
+      --volume=${PWD}:${WORK_DIRECTORY}:rw \
+      threema-desktop-env:${NODE_VERSION} \
+      /bin/bash -ic \"${args}\"
+    "
 }
+
+# Alias `node` and `npm`
 function npm {
     run_in_container npm "$@"
 }
