eb356454c2c18b3d6807fbddf0b9d72ba6cfcde6
Rob <rbr@threema.ch>
2023-05-05 10:40:26 +0200

    Improve SubstitutableText.svelte and allow nested usage

--- src/app/ui/SubstitutableText.svelte
+++ src/app/ui/SubstitutableText.svelte
@@ -1,36 +1,116 @@
 <script lang="ts">
-  export let text: string;
+  import {globals} from '~/app/globals';
+  import {type u53} from '~/common/types';
+  import {assertUnreachable, unreachable} from '~/common/utils/assert';
 
-  const splitterRegex =
-    // eslint-disable-next-line threema/ban-stateful-regex-flags
-    /<(?<voidTag>[12345])\/>|<(?<tag>[12345])>(?<text>[^<]*)<\/\k<tag>>|(?<plain>.+?)/gu;
+  export let text: string | undefined;
 
-  $: fragments = [...text.matchAll(splitterRegex)].map(({groups}) => ({
-    plain: groups?.plain,
-    tag: groups?.tag ?? groups?.voidTag,
-    isVoidTag: groups?.voidTag !== undefined,
-    text: groups?.text,
-  }));
+  const log = globals.unwrap().uiLogging.logger('ui.component.substitutable-text');
+
+  // For now there are no instances of needing more than 3 different tags in a text. We can add more
+  // if needed.
+  const ALLOWED_TAGS = ['1', '2', '3'] as const;
+  type AllowedTag = (typeof ALLOWED_TAGS)[u53];
+
+  function isAllowedTag(tag: string): tag is AllowedTag {
+    return (ALLOWED_TAGS as readonly string[]).includes(tag);
+  }
+
+  const ALLOWED_TAGS_CHAR_SET = `[${ALLOWED_TAGS.join('')}]`;
+  const SELF_CLOSING_TAG_PATTERN = `<(?<selfClosingTag>${ALLOWED_TAGS_CHAR_SET}) ?/>` as const;
+  const TAG_PATTERN = `<(?<tag>${ALLOWED_TAGS_CHAR_SET})>(?<text>.*?)</\\k<tag>>` as const;
+  const PLAIN_TEXT_PATTERN = `(?<plain>(?:.+?(?=<${ALLOWED_TAGS_CHAR_SET}(?: ?/)?>)|.+$))` as const;
+
+  // eslint-disable-next-line threema/ban-stateful-regex-flags
+  const TAG_SPLITTER_REGEX = new RegExp(
+    [SELF_CLOSING_TAG_PATTERN, TAG_PATTERN, PLAIN_TEXT_PATTERN].join('|'),
+    'gu',
+  );
+
+  type Fragment =
+    | {readonly type: 'plain'; readonly text: string}
+    | {readonly type: 'tag'; readonly tag: AllowedTag; readonly text: string}
+    | {readonly type: 'selfClosingTag'; readonly tag: AllowedTag; readonly text: undefined};
+
+  function warnMissingSlot(tag: AllowedTag): void {
+    log.warn(
+      `Text "${text}" expects a child slot with \`name="${tag}"\` but it has not been provided.`,
+    );
+  }
+
+  function warnUnusedSlots(newFragments: Fragment[]): void {
+    const expectedTags = new Set(
+      newFragments
+        .map((fragment) => (fragment.type === 'plain' ? '' : fragment.tag))
+        .filter(isAllowedTag),
+    );
+    for (const tag of ALLOWED_TAGS) {
+      if ($$slots[tag] && !expectedTags.has(tag)) {
+        log.warn(`Unused child slot with \`name="${tag}"\` for text "${text}".`);
+      }
+    }
+  }
+
+  $: fragments =
+    text === undefined
+      ? []
+      : [...text.matchAll(TAG_SPLITTER_REGEX)].map<Fragment>((match) => {
+          const matchedText = match[0];
+          const {groups} = match;
+          if (groups === undefined) {
+            return assertUnreachable('TAG_SPLITTER_REGEX should have returned a matched group');
+          }
+          if (groups.plain !== undefined) {
+            return {
+              type: 'plain',
+              text: groups.plain,
+            };
+          }
+          if (isAllowedTag(groups.tag)) {
+            if ($$slots[groups.tag]) {
+              return {
+                type: 'tag',
+                tag: groups.tag,
+                text: groups.text,
+              };
+            }
+            warnMissingSlot(groups.tag);
+            return {
+              type: 'plain',
+              text: import.meta.env.DEBUG ? matchedText : groups.text,
+            };
+          }
+          if (isAllowedTag(groups.selfClosingTag)) {
+            if ($$slots[groups.selfClosingTag]) {
+              return {
+                type: 'selfClosingTag',
+                tag: groups.selfClosingTag,
+              };
+            }
+            warnMissingSlot(groups.selfClosingTag);
+            return {
+              type: 'plain',
+              text: import.meta.env.DEBUG ? matchedText : '',
+            };
+          }
+          return assertUnreachable(`Unexpected matching by TAG_SPLITTER_REGEX on '${matchedText}'`);
+        });
+
+  $: warnUnusedSlots(fragments);
 </script>
 
 <template>
   {#each fragments as fragment}
-    {#if fragment.plain !== undefined}
-      {fragment.plain}
-    {:else if fragment.tag === '1' && $$slots[1]}
+    {#if fragment.type === 'plain'}
+      {fragment.text}
+    {:else if fragment.tag === '1'}
       <slot name="1" text={fragment.text} />
-    {:else if fragment.tag === '2' && $$slots[2]}
+    {:else if fragment.tag === '2'}
       <slot name="2" text={fragment.text} />
-    {:else if fragment.tag === '3' && $$slots[3]}
+    {:else if fragment.tag === '3'}
       <slot name="3" text={fragment.text} />
-    {:else if fragment.tag === '4' && $$slots[4]}
-      <slot name="4" text={fragment.text} />
-    {:else if fragment.tag === '5' && $$slots[5]}
-      <slot name="5" text={fragment.text} />
-    {:else if fragment.isVoidTag}
-      {`<${fragment.tag}/>`}
     {:else}
-      {`<${fragment.tag}>${fragment.text}</${fragment.tag}>`}
+      {unreachable(fragment.tag)}
     {/if}
   {/each}
 </template>
