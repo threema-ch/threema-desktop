5e10082511e0d8c1c28b81f079d2cf2c26955546
Lenny <lennart.grahl@threema.ch>
2023-05-08 00:26:22 +0200

    Add libthreema as library (currently copy pasted)

---
+++ libs/libthreema/libthreema_wasm.d.ts
@@ -0,0 +1,118 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function initLogging(): void;
+export type PathStateUpdate = { state: "awaiting-nominate"; measuredRttMs: number } | { state: "nominated"; rph: Uint8Array };
+
+export interface PathProcessResult {
+    stateUpdate: PathStateUpdate | undefined;
+    outgoingFrame: Uint8Array | undefined;
+    incomingUlpData: Uint8Array | undefined;
+}
+
+export type InitialOutgoingFrames = InitialOutgoingFrame[];
+
+export interface InitialOutgoingFrame {
+    pid: number;
+    frame: Uint8Array;
+}
+
+/**
+*/
+export class RendezvousProtocol {
+  free(): void;
+/**
+* @param {boolean} is_nominator
+* @param {Uint8Array} ak
+* @param {Uint32Array} pids
+* @returns {RendezvousProtocol}
+*/
+  static newAsRid(is_nominator: boolean, ak: Uint8Array, pids: Uint32Array): RendezvousProtocol;
+/**
+* @param {boolean} is_nominator
+* @param {Uint8Array} ak
+* @param {Uint32Array} pids
+* @returns {RendezvousProtocol}
+*/
+  static newAsRrd(is_nominator: boolean, ak: Uint8Array, pids: Uint32Array): RendezvousProtocol;
+/**
+* @returns {InitialOutgoingFrames | undefined}
+*/
+  initialOutgoingFrames(): InitialOutgoingFrames | undefined;
+/**
+* @returns {boolean}
+*/
+  isNominator(): boolean;
+/**
+* @returns {number | undefined}
+*/
+  nominatedPath(): number | undefined;
+/**
+* @param {number} pid
+* @param {Uint8Array} chunk
+*/
+  addChunk(pid: number, chunk: Uint8Array): void;
+/**
+* @param {number} pid
+* @returns {PathProcessResult}
+*/
+  processFrame(pid: number): PathProcessResult;
+/**
+* @param {number} pid
+* @returns {PathProcessResult}
+*/
+  nominatePath(pid: number): PathProcessResult;
+/**
+* @param {Uint8Array} outgoing_data
+* @returns {PathProcessResult}
+*/
+  createUlpFrame(outgoing_data: Uint8Array): PathProcessResult;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly initLogging: () => void;
+  readonly __wbg_rendezvousprotocol_free: (a: number) => void;
+  readonly rendezvousprotocol_newAsRid: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly rendezvousprotocol_newAsRrd: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly rendezvousprotocol_initialOutgoingFrames: (a: number) => number;
+  readonly rendezvousprotocol_isNominator: (a: number) => number;
+  readonly rendezvousprotocol_nominatedPath: (a: number, b: number) => void;
+  readonly rendezvousprotocol_addChunk: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly rendezvousprotocol_processFrame: (a: number, b: number, c: number) => void;
+  readonly rendezvousprotocol_nominatePath: (a: number, b: number, c: number) => void;
+  readonly rendezvousprotocol_createUlpFrame: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ libs/libthreema/libthreema_wasm_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initLogging(): void;
+export function __wbg_rendezvousprotocol_free(a: number): void;
+export function rendezvousprotocol_newAsRid(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function rendezvousprotocol_newAsRrd(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function rendezvousprotocol_initialOutgoingFrames(a: number): number;
+export function rendezvousprotocol_isNominator(a: number): number;
+export function rendezvousprotocol_nominatedPath(a: number, b: number): void;
+export function rendezvousprotocol_addChunk(a: number, b: number, c: number, d: number, e: number): void;
+export function rendezvousprotocol_processFrame(a: number, b: number, c: number): void;
+export function rendezvousprotocol_nominatePath(a: number, b: number, c: number): void;
+export function rendezvousprotocol_createUlpFrame(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __externref_table_dealloc(a: number): void;
+export function __wbindgen_start(): void;
