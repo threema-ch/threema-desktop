443e972aa98be40fae1f48c87c0098bc02484218
Gian-Reto Tarnutzer <gian.reto.tarnutzer@threema.ch>
2023-05-11 15:13:40 +0200

    Fix contact navigation tab switcher layout

--- src/app/ui/nav/receiver/ReceiverTabSwitcher.svelte
+++ src/app/ui/nav/receiver/ReceiverTabSwitcher.svelte
@@ -2,6 +2,8 @@
   import MdIcon from '#3sc/components/blocks/Icon/MdIcon.svelte';
   import {type ContactTab} from '~/app/ui/nav';
   import Tab from '~/app/ui/nav/receiver/Tab.svelte';
+  import {type u53} from '~/common/types';
+  import {unreachable} from '~/common/utils/assert';
 
   export let activeTab: ContactTab;
 
@@ -12,50 +14,72 @@
   export let tmpShowGroup = true;
 
   const buildVariant = import.meta.env.BUILD_VARIANT;
+  const tabs = [
+    {
+      id: 'work-contacts',
+      icon: 'work_outline',
+    },
+    {
+      id: 'private-contacts',
+      icon: 'person',
+    },
+    {
+      id: 'groups',
+      icon: 'group',
+    },
+    {
+      id: 'distribution-lists',
+      icon: 'campaign',
+    },
+  ] as const;
+
+  function isTabVisible(tab: (typeof tabs)[u53]): boolean {
+    switch (tab.id) {
+      case 'work-contacts':
+        // The work contacts tab is only visible in work builds.
+        return buildVariant === 'work';
+
+      case 'private-contacts':
+        // The private contacts tab is always visible.
+        return true;
+
+      case 'groups':
+        // TODO(DESK-830): The groups tab is controlled by `tmpShowGroup`. Update this if said
+        // variable is removed.
+        return tmpShowGroup;
+
+      case 'distribution-lists':
+        // TODO(DESK-236): Currently never displayed. Display distribution list again after the
+        // model has been introduced.
+        return false;
+
+      default:
+        return unreachable(tab);
+    }
+  }
+
+  $: visibleTabs = tabs.filter(isTabVisible);
 </script>
 
 <template>
-  <div data-variant={buildVariant} data-groups={tmpShowGroup}>
-    {#if buildVariant === 'work'}
-      <Tab tab="work-contacts" bind:activeTab>
-        <MdIcon theme="Outlined">work_outline</MdIcon>
-      </Tab>
-    {/if}
-    <Tab tab="private-contacts" bind:activeTab>
-      <MdIcon theme="Outlined">person</MdIcon>
-    </Tab>
-    {#if tmpShowGroup}
-      <Tab tab="groups" bind:activeTab>
-        <MdIcon theme="Outlined">group</MdIcon>
-      </Tab>
-    {/if}
-    <!-- // TODO(DESK-236): Display distribution list again after the model has been introduced -->
-    <!-- <Tab tab="distribution-lists" bind:activeTab>
-      <MdIcon theme="Outlined">campaign</MdIcon>
-    </Tab> -->
-  </div>
+  {#if visibleTabs.length >= 2}
+    <div style={`grid-template-columns: repeat(${visibleTabs.length}, 1fr);`}>
+      {#each visibleTabs as tab}
+        <Tab tab={tab.id} bind:activeTab>
+          <MdIcon theme="Outlined">{tab.icon}</MdIcon>
+        </Tab>
+      {/each}
+    </div>
+  {/if}
 </template>
 
 <style lang="scss">
   @use 'component' as *;
   div {
     display: grid;
-    // TODO(DESK-236): Display distribution list again after the model has been introduced
-    // grid-template: 'contacts groups distributionlists';
-    grid-template: 'contacts';
     place-items: center;
     justify-items: stretch;
     user-select: none;
     --c-icon-font-size: #{rem(24px)};
-
-    &[data-groups='true'] {
-      grid-template: 'contacts groups';
-    }
-
-    &[data-variant='work'] {
-      // TODO(DESK-236): Display distribution list again after the model has been introduced
-      // grid-template: 'work contacts groups distributionlists';
-      grid-template: 'work contacts groups';
-    }
   }
 </style>
