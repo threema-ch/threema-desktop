4fd6a13028c609f958f533573959b6f81e5c05ff
Rob <rbr@threema.ch>
2023-06-19 13:38:43 +0200

    Simplify isContactBlocked check in incoming CSP messages

--- src/common/network/protocol/task/csp/incoming-message.ts
+++ src/common/network/protocol/task/csp/incoming-message.ts
@@ -345,7 +345,6 @@ export class IncomingMessageTask implements ActiveTask<void, 'volatile'> {
     public readonly transaction = undefined;
     private readonly _log: Logger;
     private readonly _id: MessageId;
-    private _isContactBlocked: (identityString: IdentityString) => boolean;
 
     public constructor(
         private readonly _services: ServicesForTasks,
@@ -354,12 +353,6 @@ export class IncomingMessageTask implements ActiveTask<void, 'volatile'> {
         const messageIdHex = u64ToHexLe(_message.messageId);
         this._log = _services.logging.logger(`network.protocol.task.in-message.${messageIdHex}`);
         this._id = ensureMessageId(this._message.messageId);
-
-        const privacySettings = _services.model.user.privacySettings;
-        this._isContactBlocked = (i) => privacySettings.get().controller.isContactBlocked(i);
-        privacySettings.subscribe((ps) => {
-            this._isContactBlocked = (i) => ps.controller.isContactBlocked(i);
-        });
     }
 
     public async run(handle: ActiveTaskCodecHandle<'volatile'>): Promise<void> {
@@ -474,7 +467,10 @@ export class IncomingMessageTask implements ActiveTask<void, 'volatile'> {
 
         // Check if the message should be discarded due to the contact being
         // implicitly or explicitly blocked.
-        if (!BLOCK_EXEMPTION_TYPES.has(type) && this._isContactBlocked(sender.string)) {
+        if (
+            !BLOCK_EXEMPTION_TYPES.has(type) &&
+            model.user.privacySettings.get().controller.isContactBlocked(sender.string)
+        ) {
             this._log.info(`Discarding message from blocked contact ${sender.string}`);
             return await this._discard(handle);
         }
