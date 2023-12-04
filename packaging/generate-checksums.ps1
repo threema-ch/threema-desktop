dc0cea0f0cf7f1417643895adc46d2e2f430f30f
Danilo Bargen <danilo.bargen@threema.ch>
2023-12-04 09:31:35 +0100

    Packaging: Avoid truncating Windows release hashes

--- packaging/generate-checksums.ps1
+++ packaging/generate-checksums.ps1
@@ -6,6 +6,7 @@ param(
     [String]$FilePath
 )
 
+# Note: The "-Width 99999" parameter is used to avoid truncation of hash
 Get-ChildItem "$FilePath" |
     Select-Object -Property Name,Length,@{name="SHA256";expression={(Get-FileHash -Algorithm SHA256 $_.FullName).hash}} |
-    Out-File -FilePath "$FilePath.sha256"
+    Out-File -Width 99999 -FilePath "$FilePath.sha256"
