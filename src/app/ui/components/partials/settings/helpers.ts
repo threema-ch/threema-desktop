ac9ac32c2d515624702749b43e9344241b13e961
Danilo Bargen <danilo.bargen@threema.ch>
2023-12-15 16:41:01 +0100

    Simplify implementation of createDropdownItems

--- src/app/ui/components/partials/settings/helpers.ts
+++ src/app/ui/components/partials/settings/helpers.ts
@@ -1,8 +1,6 @@
-import type {
-    SettingsDropdown,
-    SettingsDropdownItem,
-} from '~/app/ui/components/partials/settings/types';
+import type {SettingsDropdown} from '~/app/ui/components/partials/settings/types';
 import type {ContextMenuItem} from '~/app/ui/utils/context-menu/types';
+import type {u53} from '~/common/types';
 
 /**
  * This functions creates a list of dropdownitems that can be used in a contextmenu
@@ -15,21 +13,11 @@ export function createDropdownItems<TSettings, TSettingType>(
     dropdown: SettingsDropdown<TSettings, TSettingType>,
     handler: (newValue: TSettingType, newLabel: keyof TSettings) => void,
 ): ContextMenuItem[] {
-    const dropdownItems: ContextMenuItem[] = [];
-    Object.entries<SettingsDropdownItem<TSettingType, keyof TSettings>>({...dropdown}).forEach(
-        ([value, item]) => {
-            function cb(): void {
-                handler(item.value, item.label);
-            }
-            const contextMenuItem: ContextMenuItem = {
-                handler: cb,
-                icon: item.options?.icon ?? undefined,
-                label: item.text,
-            };
+    type DropdownItem = (typeof dropdown)[u53];
 
-            dropdownItems.push(contextMenuItem);
-        },
-    );
-
-    return dropdownItems;
+    return Object.values<DropdownItem>({...dropdown}).map((item) => ({
+        handler: () => handler(item.value, item.label),
+        icon: item.options?.icon ?? undefined,
+        label: item.text,
+    }));
 }
