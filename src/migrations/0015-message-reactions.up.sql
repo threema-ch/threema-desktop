876cad1246bc257cf7b06ab40a0d7b74f20f12a8
Danilo Bargen <danilo.bargen@threema.ch>
2024-01-11 16:16:06 +0100

    Small improvements to migration

--- src/migrations/0015-message-reactions.up.sql
+++ src/migrations/0015-message-reactions.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE messageReactions (
     reactionAt INTEGER NOT NULL,
     -- 0: Acknowledged, 1: Declined
     reaction INTEGER NOT NULL,
-    -- We use the sender contect's identity instead of the uid to be able to show reactions of
+    -- We use the sender contact's identity instead of the uid to be able to show reactions of
     -- people that have left the group. The special string "me" is used to indicate that the user is
     -- the reaction sender; `NULL` is not used for this due to its behavior in unique constraints.
     senderContactIdentity TEXT NOT NULL,
@@ -23,15 +23,14 @@ SELECT
     lastReaction,
     -- Until now, it was only possible to react to inbound messages, which allows the following
     -- assumptions:
-    --     - If the message was outbound, the reaction has to be from a contact, and needs to be 
+    --     - If the message was outbound, the reaction has to be from a contact, and needs to be
     --       looked up.
-    --     - If the message was inbound, the reaction was by the user themself, and will be marked 
+    --     - If the message was inbound, the reaction was by the user themself, and will be marked
     --       using the special string "me".
     CASE WHEN senderContactUid IS NULL THEN
-        (SELECT identity FROM contacts 
+        (SELECT identity FROM contacts
             WHERE contacts.uid =
-                (SELECT contactUid from conversations WHERE uid = conversationUid)) ELSE 'me' END,
+                (SELECT contactUid FROM conversations WHERE uid = conversationUid)) ELSE 'me' END,
     uid
 FROM messages
--- When `lastReaction` is not `NULL`, `lastReactionAt` can also be expected to be defined
-WHERE messages.lastReaction IS NOT NULL;
+WHERE messages.lastReaction IS NOT NULL AND messages.lastReactionAt IS NOT NULL;
