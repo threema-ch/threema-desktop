5d74bd5a427790b370bf65537ecafdbec8edc119
Gian-Reto Tarnutzer <gian.reto.tarnutzer@threema.ch>
2024-02-02 17:02:27 +0100

    Remove unused files from #3sc import

--- src/app/ui/svelte-components/types.ts
+++
@@ -1,9 +0,0 @@
-import type {u53} from '~/common/types';
-
-/**
- * Dimensions for a 2D pixel-based object.
- */
-export interface Dimensions {
-    width: u53;
-    height: u53;
-}
--- src/app/ui/svelte-components/utils/threema.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns whether the given `string` is a syntactically correct Threema ID. Note: This doesn't
- * verify if the ID is actually valid.
- */
-export function isThreemaId(threemaId: string): boolean {
-    return threemaId.match(/^(?<id>(?<work>#[A-Z0-9]{7})|(?<private>[A-Z0-9]{8}))$/u) !== null;
-}
