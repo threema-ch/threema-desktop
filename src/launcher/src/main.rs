df4acadd31519f5ade5eb2b116fccc68b932c5b3
Danilo Bargen <danilo.bargen@threema.ch>
2024-03-13 16:28:12 +0100

    Launcher: Determine target binary name, support colors on Windows
    The path can optionally still be overridden (if the appropriate cargo
    feature is enabled), but will otherwise be auto-detected.
    
    This improves security and simplifies packaging under Windows. (We still
    need customized paths for Flatpak though.)

--- src/launcher/src/main.rs
+++ src/launcher/src/main.rs
@@ -2,13 +2,17 @@ use std::{
     env, fs,
     path::{Path, PathBuf},
     process::{self, Command, Output, Stdio},
+    time::Duration,
 };
 
+use colored::Colorize;
 use home::home_dir;
 
 // Compile-time constants
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const BUILD_FLAVOR: &str = env!("THREEMA_BUILD_FLAVOR"); // e.g. consumer-sandbox or work-live
+
+// Valid build flavors
 const VALID_BUILD_FLAVORS: [&str; 5] = [
     "consumer-sandbox",
     "consumer-live",
@@ -22,27 +26,46 @@ const EXIT_CODE_RESTART: i32 = 8;
 const EXIT_CODE_DELETE_PROFILE_AND_RESTART: i32 = 9;
 const EXIT_CODE_LAUNCHER_ERROR: i32 = 20;
 
+// Delays
+const DELAY_BEFORE_ERROR_EXIT_MS: u64 = 2000;
+
+#[cfg(windows)]
+fn init_terminal() {
+    let _ = colored::control::set_virtual_terminal(true);
+}
+
+#[cfg(not(windows))]
+fn init_terminal() {}
+
 /// Print an error in red.
 #[macro_export]
 macro_rules! print_error {
     ($msg:expr) => {{
-        eprint!("\x1b[91m");
-        eprint!($msg);
-        eprintln!("\x1b[0m");
+        eprintln!("{}", $msg.bright_red());
     }};
     ($msg:expr, $($args:expr),* $(,)?) => {{
-        eprint!("\x1b[91m");
-        eprint!($msg, $($args),*);
-        eprintln!("\x1b[0m");
+        eprintln!("{}", format!($msg, $($args),*).bright_red());
     }}
 }
 
 fn print_usage_and_exit(launcher_path: &str) -> ! {
-    print_error!(
-        "Usage: {} [-h|--help] [--version] <path-to-threema-desktop> [args...]",
-        launcher_path
-    );
+    if cfg!(feature = "allow_path_override") {
+        print_error!(
+            "Usage: {} [-h|--help] [--version] [--target-bin <path>] [args...]",
+            launcher_path
+        );
+    } else {
+        print_error!("Usage: {} [-h|--help] [--version] [args...]", launcher_path);
+    }
     process::exit(EXIT_CODE_LAUNCHER_ERROR);
+}
+
+/// Determine binary name based on operating system
+fn determine_binary_name() -> &'static str {
+    match env::consts::OS {
+        "windows" => "ThreemaDesktop.exe",
+        _other => "ThreemaDesktop",
+    }
 }
 
 /// Determine profile directory location based on operating system:
@@ -108,6 +131,8 @@ fn determine_profile_directory(args: &[String]) -> PathBuf {
 }
 
 fn main() {
+    init_terminal();
+
     // Assertions
     assert!(
         VALID_BUILD_FLAVORS.contains(&BUILD_FLAVOR),
@@ -115,15 +140,15 @@ fn main() {
     );
 
     // Print header
-    if BUILD_FLAVOR.starts_with("work-") {
-        print!("\x1b[34m");
+    let color = if BUILD_FLAVOR.starts_with("work-") {
+        "blue"
     } else {
-        print!("\x1b[32m");
-    }
-    println!(" _____ _                         ");
-    println!("|_   _| |_ ___ ___ ___ _____ ___ ");
-    println!("  | | |   |  _| -_| -_|     | .'|");
-    println!("  |_| |_|_|_| |___|___|_|_|_|__,|\x1b[0m");
+        "green"
+    };
+    println!("{}", " _____ _                         ".color(color));
+    println!("{}", "|_   _| |_ ___ ___ ___ _____ ___ ".color(color));
+    println!("{}", "  | | |   |  _| -_| -_|     | .'|".color(color));
+    println!("{}", "  |_| |_|_|_| |___|___|_|_|_|__,|".color(color));
     println!();
     println!("Desktop launcher v{VERSION} ({BUILD_FLAVOR})");
 
@@ -134,24 +159,47 @@ fn main() {
     let launcher_path = args.remove(0);
 
     // Show help or version
-    if args.is_empty() || args.iter().any(|arg| arg == "-h" || arg == "--help") {
+    if args.iter().any(|arg| arg == "-h" || arg == "--help") {
         print_usage_and_exit(&launcher_path);
     }
     if args.iter().any(|arg| arg == "--version") {
         process::exit(0);
     }
 
-    // Extract target binary path
-    let target_path = PathBuf::from(args.remove(0));
+    // Determine target binary path.
+    //
+    // If overridden through "--target-bin <path>" parameter, use that path.
+    // Otherwise, fall back to a default binary name adjacent to the launcher.
+    let mut target_path_override = None;
+    if cfg!(feature = "allow_path_override") {
+        if let Some(i) = args.iter().position(|arg| arg == "--target-bin") {
+            if args.len() > i + 1 {
+                args.remove(i);
+                target_path_override = Some(args.remove(i));
+            }
+        }
+    }
+    let target_path = match env::current_exe()
+        .ok()
+        .and_then(|path| path.parent().map(Path::to_path_buf))
+    {
+        Some(dir) => {
+            dir.join(target_path_override.unwrap_or_else(|| determine_binary_name().to_string()))
+        }
+        None => {
+            print_error!("Could not determine current executable directory");
+            process::exit(EXIT_CODE_LAUNCHER_ERROR);
+        }
+    };
     println!("Launching Threema Desktop through {target_path:?}");
 
     // Ensure that target path exists
     if !target_path.exists() {
-        print_error!("Error: Target path {target_path:?} not found");
+        print_error!("Error: Target path {:?} not found", target_path);
         process::exit(EXIT_CODE_LAUNCHER_ERROR);
     }
     if !target_path.is_file() {
-        print_error!("Error: Target path {target_path:?} is not a file");
+        print_error!("Error: Target path {:?} is not a file", target_path);
         process::exit(EXIT_CODE_LAUNCHER_ERROR);
     }
 
@@ -175,7 +223,7 @@ fn main() {
                 status.code()
             }
             Err(e) => {
-                print_error!("Failed to launch target binary: {e}");
+                print_error!("Failed to launch target binary: {}", e);
                 process::exit(EXIT_CODE_LAUNCHER_ERROR);
             }
         };
@@ -189,16 +237,28 @@ fn main() {
             }
             Some(EXIT_CODE_DELETE_PROFILE_AND_RESTART) => {
                 println!("------");
-                println!("\x1b[33mRemoving profile directory at {profile_directory:?}!\x1b[0m");
+                println!(
+                    "{}",
+                    format!("Removing profile directory at {profile_directory:?}!").yellow()
+                );
                 if let Err(e) = fs::remove_dir_all(&profile_directory) {
-                    print_error!("Failed to remove profile directory: {e:#}");
+                    print_error!("Failed to remove profile directory: {:#}", e);
+                    std::thread::sleep(Duration::from_millis(DELAY_BEFORE_ERROR_EXIT_MS));
                     process::exit(EXIT_CODE_LAUNCHER_ERROR);
                 }
                 println!("Restarting");
                 continue;
             }
-            Some(other) => process::exit(other),
-            None => process::exit(EXIT_CODE_LAUNCHER_ERROR),
+            Some(other) => {
+                print_error!("Unexpected exit code: {}", other);
+                std::thread::sleep(Duration::from_millis(DELAY_BEFORE_ERROR_EXIT_MS));
+                process::exit(other);
+            }
+            None => {
+                print_error!("Missing exit code");
+                std::thread::sleep(Duration::from_millis(DELAY_BEFORE_ERROR_EXIT_MS));
+                process::exit(EXIT_CODE_LAUNCHER_ERROR);
+            }
         }
     }
 }
