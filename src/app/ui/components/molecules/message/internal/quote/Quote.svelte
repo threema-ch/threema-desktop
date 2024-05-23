5f0a96f82b4aefa2e348812d66cd8d9b7ce8decd
Gian-Reto Tarnutzer <gian.reto.tarnutzer@threema.ch>
2024-05-23 16:29:01 +0200

    Remove redundant styles from edit message UI

--- src/app/ui/components/molecules/message/internal/quote/Quote.svelte
+++ src/app/ui/components/molecules/message/internal/quote/Quote.svelte
@@ -5,6 +5,7 @@
 <script lang="ts">
   import LazyImage from '~/app/ui/components/atoms/lazy-image/LazyImage.svelte';
   import Prose from '~/app/ui/components/atoms/prose/Prose.svelte';
+  import Text from '~/app/ui/components/atoms/text/Text.svelte';
   import AudioPlayer from '~/app/ui/components/molecules/audio-player/AudioPlayer.svelte';
   import FileInfo from '~/app/ui/components/molecules/message/internal/file-info/FileInfo.svelte';
   import type {QuoteProps} from '~/app/ui/components/molecules/message/internal/quote/props';
@@ -42,8 +43,13 @@
       <Sender name={sender.name} color={sender.color} />
     </span>
   {:else if mode === 'edit'}
-    <span class="sender">
-      <Sender name={$i18n.t('messaging.prose--edit-message', 'Edit Message')} color={'none'} />
+    <span class="title">
+      <Text
+        family="primary"
+        selectable={true}
+        size="body-small"
+        text={$i18n.t('messaging.prose--edit-message', 'Edit Message')}
+      />
     </span>
   {/if}
 
@@ -107,14 +113,15 @@
 <style lang="scss">
   @use 'component' as *;
 
-  .quote {
+  .quote,
+  .edit {
     @extend %neutral-input;
     text-align: start;
 
     position: relative;
     display: grid;
     grid-template:
-      'sender' min-content
+      'title' min-content
       'preview' 1fr
       / auto;
     align-items: center;
@@ -126,14 +133,14 @@
 
     &.captioned {
       grid-template:
-        'sender preview' min-content
+        'title preview' min-content
         'text preview' 1fr
         / auto min-content;
 
       &.audio-container,
       &.file-container {
         grid-template:
-          'sender' min-content
+          'title' min-content
           'preview' 1fr
           'text' min-content
           / auto;
@@ -147,24 +154,6 @@
         justify-content: start;
         color: var(--mc-message-quote-text-color);
       }
-    }
-
-    &:before {
-      content: '';
-      position: absolute;
-      display: block;
-      left: 0;
-      top: rem(5px);
-      width: var(--mc-message-quote-border-width);
-      height: calc(100% - rem(10px));
-      border-radius: calc(var(--mc-message-quote-border-width) / 2);
-      // Default quote bar is the same as the text color.
-      background-color: var(--mc-message-quote-text-color);
-    }
-
-    .sender {
-      grid-area: sender;
-      line-height: 1em;
     }
 
     .thumbnail {
@@ -219,101 +208,32 @@
     }
   }
 
-  .edit {
-    @extend %neutral-input;
-    text-align: start;
-
-    position: relative;
-    display: grid;
-    grid-template:
-      'sender' min-content
-      'preview' 1fr
-      / auto;
-    align-items: center;
-    column-gap: rem(24px);
-    row-gap: 0;
-    min-height: rem(40px);
-    padding-left: rem(10px);
-    width: 100%;
-
-    &.captioned {
-      grid-template:
-        'sender preview' min-content
-        'text preview' 1fr
-        / auto min-content;
-
-      &.audio-container,
-      &.file-container {
-        grid-template:
-          'sender' min-content
-          'preview' 1fr
-          'text' min-content
-          / auto;
-        row-gap: rem(4px);
-      }
-
-      .text {
-        grid-area: text;
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        color: var(--mc-message-quote-text-color);
-      }
+  .quote {
+    &:before {
+      content: '';
+      position: absolute;
+      display: block;
+      left: 0;
+      top: rem(5px);
+      width: var(--mc-message-quote-border-width);
+      height: calc(100% - rem(10px));
+      border-radius: calc(var(--mc-message-quote-border-width) / 2);
+      // Default quote bar is the same as the text color.
+      background-color: var(--mc-message-quote-text-color);
     }
+
     .sender {
-      grid-area: sender;
+      grid-area: title;
       line-height: 1em;
     }
+  }
 
-    .thumbnail {
-      position: relative;
-      grid-area: preview;
-      width: min-content;
-      height: min-content;
-      border-radius: rem(8px);
-      overflow: hidden;
+  .edit {
+    padding-left: 0;
 
-      .play-icon {
-        @include clicktarget-button-circle;
-        pointer-events: none;
-        display: flex;
-        position: absolute;
-        justify-content: center;
-        align-items: center;
-        color: var(--mc-message-overlay-button-color);
-        background-color: var(--mc-message-overlay-button-background-color);
-        width: rem(22px);
-        height: rem(22px);
-        left: calc(50% - rem(11px));
-        top: calc(50% - rem(11px));
-        font-size: rem(12px);
-
-        --c-icon-button-naked-outer-background-color--hover: var(
-          --mc-message-overlay-button-background-color--hover
-        );
-        --c-icon-button-naked-outer-background-color--focus: var(
-          --mc-message-overlay-button-background-color--focus
-        );
-        --c-icon-button-naked-outer-background-color--active: var(
-          --mc-message-overlay-button-background-color--active
-        );
-      }
-    }
-
-    .audio {
-      grid-area: preview;
-    }
-
-    &:not(.captioned) {
-      .thumbnail,
-      .audio {
-        margin: rem(4px) 0;
-      }
-    }
-
-    &:not(:disabled),
-    &:global(:not(:disabled) *) {
-      cursor: pointer;
+    .title {
+      grid-area: title;
+      line-height: 1em;
     }
   }
 
