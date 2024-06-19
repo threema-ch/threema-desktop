0ba88ac0f6181d5cb9a8cd148be68d5e4434fc10
André Gaillard <andre.gaillard@threema.ch>
2024-06-19 15:49:57 +0200

    Add a function that checks if the restored messages were restored correctly

--- src/common/dom/backend/restore-db.ts
+++ src/common/dom/backend/restore-db.ts
@@ -10,13 +10,13 @@ import type {
     RawDatabaseKey,
 } from '~/common/db';
 import type {FactoriesForBackend} from '~/common/dom/backend';
-import {MessageType} from '~/common/enum';
+import {MessageType, ReceiverType} from '~/common/enum';
 import {canCopyFiles, copyFilesInBackground, type FileId} from '~/common/file-storage';
 import type {ServicesForKeyStorage} from '~/common/key-storage';
 import type {Logger} from '~/common/logging';
 import type {IdentityString} from '~/common/network/types';
 import type {u53} from '~/common/types';
-import {assertUnreachable, unreachable} from '~/common/utils/assert';
+import {assert, assertUnreachable, unreachable} from '~/common/utils/assert';
 
 /**
  * Checks the password entered by the user against an old profile.
@@ -273,6 +273,12 @@ export function transferOldMessages(
         }
     } while (statusMessages.length !== 0);
 
+    // TODO(DESK-1503) Revert the commit that added this comment.
+    if (import.meta.env.BUILD_ENVIRONMENT === 'sandbox') {
+        log.debug('Testing database consistency');
+        testConsistency(db, oldDb);
+    }
+
     // Refresh the cache to directly display the conversations again.
     model.conversations.refreshCache();
 }
@@ -293,4 +299,179 @@ function restoreReactionsAndHistory(
             editedAt: historyEntry.editedAt,
         });
     });
+}
+
+/**
+ * This function compares two databases against each other, checking that the messages have the same
+ * content, createdAt timestamp and that they are in the same conversation.
+ *
+ * TODO(DESK-1503) Revert the commit that added this comment.
+ *
+ */
+function testConsistency(newDatabase: DatabaseBackend, oldDatabase: DatabaseBackend): void {
+    const chunkSize = 1000;
+    let offset = 0;
+    let messages = [];
+    do {
+        messages = newDatabase.getMessages({limit: chunkSize, offset});
+        offset += chunkSize;
+
+        for (const message of messages) {
+            // We need to check that we are in the same conversation. The conversation must
+            // exist in the old database as well since we base it on messages, and all messages at
+            // this point must have come from the backup.
+            const conversation = newDatabase.getConversationByUid(message.conversationUid);
+            assert(conversation !== undefined, 'Conversation must exist in new database');
+            let conversationUidInOldDb;
+            switch (conversation.receiver.type) {
+                case ReceiverType.CONTACT: {
+                    const receiverUid = conversation.receiver.uid;
+                    const contactIdentity = newDatabase.getContactByUid(receiverUid)?.identity;
+                    assert(contactIdentity !== undefined, 'Contact must exist in new database');
+                    conversationUidInOldDb =
+                        oldDatabase.getContactConversationUidByIdentity(contactIdentity);
+                    assert(
+                        conversationUidInOldDb !== undefined,
+                        'Single Conversation must exist in old database',
+                    );
+                    const conversationInOldDb =
+                        oldDatabase.getConversationByUid(conversationUidInOldDb);
+                    assert(
+                        conversationInOldDb !== undefined,
+                        'Conversation must exist in old database',
+                    );
+                    assert(
+                        conversationInOldDb.receiver.type === ReceiverType.CONTACT,
+                        'Receiver type must be consistent in both databases',
+                    );
+                    assert(
+                        contactIdentity ===
+                            oldDatabase.getContactByUid(conversationInOldDb.receiver.uid)?.identity,
+                        'Contact identity must be consistent in both databases',
+                    );
+                    break;
+                }
+                case ReceiverType.GROUP: {
+                    const receiverUid = conversation.receiver.uid;
+                    const group = newDatabase.getGroupByUid(receiverUid);
+                    assert(group !== undefined, 'Group must exist in new database');
+                    const {groupId, creatorUid} = group;
+                    let creatorIdentity = undefined;
+                    if (creatorUid !== undefined) {
+                        creatorIdentity = newDatabase.getContactByUid(creatorUid)?.identity;
+                        assert(
+                            creatorIdentity !== undefined,
+                            'Cannot have a group in old database with an unknown creator',
+                        );
+                    }
+                    conversationUidInOldDb = oldDatabase.getGroupConversationUidByCreatorIdentity(
+                        creatorIdentity,
+                        groupId,
+                    );
+                    assert(
+                        conversationUidInOldDb !== undefined,
+                        'Group Conversation must exist in old database',
+                    );
+                    const conversationInOldDb =
+                        oldDatabase.getConversationByUid(conversationUidInOldDb);
+                    assert(
+                        conversationInOldDb !== undefined,
+                        'Conversation must exist in old database',
+                    );
+                    assert(conversationInOldDb.receiver.type === ReceiverType.GROUP);
+                    const groupInOldDb = oldDatabase.getGroupByUid(
+                        conversationInOldDb.receiver.uid,
+                    );
+                    assert(
+                        groupInOldDb !== undefined,
+                        'Group with messages must exist in old database',
+                    );
+
+                    let oldDbCreatorIdentity = undefined;
+                    if (groupInOldDb.creatorUid !== undefined) {
+                        oldDbCreatorIdentity = oldDatabase.getContactByUid(
+                            groupInOldDb.creatorUid,
+                        )?.identity;
+                        assert(
+                            oldDbCreatorIdentity !== undefined,
+                            'Contact of creator must exist in old database',
+                        );
+                    }
+
+                    assert(
+                        creatorIdentity === oldDbCreatorIdentity,
+                        'Group creator identity must be consistent in both databases',
+                    );
+                    assert(
+                        groupId === groupInOldDb.groupId,
+                        'Group Id identity must be consistent in both databases',
+                    );
+
+                    break;
+                }
+                case ReceiverType.DISTRIBUTION_LIST:
+                    // TODO(DESK-236)
+                    // Should never happen
+                    continue;
+                default:
+                    unreachable(conversation.receiver);
+            }
+
+            const senderIdentity =
+                message.senderContactUid === undefined
+                    ? undefined
+                    : newDatabase.getContactByUid(message.senderContactUid)?.identity;
+
+            const messageUidInOldDatabase = oldDatabase.hasMessageById(
+                conversationUidInOldDb,
+                message.id,
+            );
+            assert(messageUidInOldDatabase !== undefined, 'Message must exist in the old database');
+
+            const messageInOldDatabase = oldDatabase.getMessageByUid(messageUidInOldDatabase);
+            assert(messageInOldDatabase !== undefined);
+            // Now we assert that the message content has stayed the same
+            assert(
+                message.createdAt.getTime() === messageInOldDatabase.createdAt.getTime(),
+                'CreatedAt must be consistent in both databases',
+            );
+            assert(
+                message.deletedAt?.getTime() === messageInOldDatabase.deletedAt?.getTime(),
+                'DeletedAt must be consistent in both databases',
+            );
+            assert(
+                message.type === messageInOldDatabase.type,
+                'Type must be consistent in both databases',
+            );
+            assert(
+                message.lastEditedAt?.getTime() ===
+                    (messageInOldDatabase.type === MessageType.DELETED
+                        ? undefined
+                        : messageInOldDatabase.lastEditedAt?.getTime()),
+                'LastEditedAt must be consistent in both databases',
+            );
+
+            if (messageInOldDatabase.senderContactUid !== undefined) {
+                const oldDbSenderIdentity = oldDatabase.getContactByUid(
+                    messageInOldDatabase.senderContactUid,
+                )?.identity;
+
+                assert(
+                    senderIdentity === oldDbSenderIdentity,
+                    'Sender must be consistent in both databases',
+                );
+            } else {
+                assert(
+                    message.senderContactUid === undefined,
+                    'Sender must be consistent in both databases, it should be the user',
+                );
+            }
+
+            // Finally check that the text is the same
+            assert(
+                message.text === oldDatabase.getMessageText(messageInOldDatabase),
+                'The text of the message must be identical',
+            );
+        }
+    } while (messages.length !== 0);
 }
