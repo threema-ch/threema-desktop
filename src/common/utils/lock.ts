1404edcab198f8c1d98ee483ce92f7443c43c254
Lenny <lennart.grahl@threema.ch>
2024-06-20 14:38:30 +0200

    Improve `AsyncLock`
    - Allow it to hold an inner value that needs to be acquired
    - Add `context` property to get the current context that acquired the
      lock, if any

--- src/common/utils/lock.ts
+++ src/common/utils/lock.ts
@@ -1,5 +1,5 @@
 /**
- * A TypeScript async lock implemented with promise chaining.
+ * An async lock implemented with promise chaining.
  *
  * @example
  * ```ts
@@ -9,29 +9,57 @@
  *   });
  * ```
  */
-export class AsyncLock {
+export class AsyncLock<TContext = undefined, TGuardedValue = undefined> {
+    private readonly _value: TGuardedValue;
     private _queue: Promise<unknown> = Promise.resolve();
+    private _context: TContext | undefined = undefined;
+
+    public constructor(
+        ...[value]: TGuardedValue extends undefined ? [] : readonly [value: TGuardedValue]
+    ) {
+        this._value = value as TGuardedValue;
+    }
+
+    /** Get the current context that acquired the {@link AsyncLock}, if any. */
+    public get context(): TContext | undefined {
+        return this._context;
+    }
 
     /**
-     * Run an async function (the `executor`) within the scope of this lock.
+     * IMPORTANT: UNSAFE! This circumvents the locking mechanism to retrieve the guarded value.
+     */
+    public unwrap(): TGuardedValue {
+        return this._value;
+    }
+
+    /**
+     * Run a function (the `executor`) within the scope of this lock.
      *
      * All enqueued executors are processed serially.
      */
     // eslint-disable-next-line @typescript-eslint/promise-function-async
-    public with<T>(executor: () => Promise<T>): Promise<T> {
-        this._queue = this._enqueue(this._queue, executor);
-        return this._queue as Promise<T>;
+    public with<TResult>(
+        executor: (value: TGuardedValue) => TResult,
+        ...[context]: TContext extends undefined ? [] : readonly [context: TContext]
+    ): TResult extends Promise<unknown> ? TResult : Promise<TResult> {
+        this._queue = this._enqueue(this._queue, executor, context);
+        return this._queue as TResult extends Promise<unknown> ? TResult : Promise<TResult>;
     }
 
-    /**
-     * Await the queue, then await and return the executor.
-     */
-    private async _enqueue<T>(queue: Promise<unknown>, executor: () => Promise<T>): Promise<T> {
+    /** Await the queue, then await the executor and return the result.*/
+    private async _enqueue<TResult>(
+        queue: Promise<unknown>,
+        executor: (value: TGuardedValue) => TResult | Promise<TResult>,
+        context: TContext | undefined,
+    ): Promise<TResult> {
         try {
             await queue;
         } catch {
             // Ignored
         }
-        return await executor();
+        this._context = context;
+        const result = await executor(this._value);
+        this._context = undefined;
+        return result;
     }
 }
