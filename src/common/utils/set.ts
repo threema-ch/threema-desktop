53f983ded0e72f7b3d8ebfc9d402df57cc4f1237
Lenny <lennart.grahl@threema.ch>
2024-06-20 14:39:14 +0200

    Add `difference` function to set functions

--- src/common/utils/set.ts
+++ src/common/utils/set.ts
@@ -3,3 +3,17 @@
  */
 export type SetValue<TSet extends ReadonlySet<unknown>> =
     TSet extends ReadonlySet<infer TSetValue> ? TSetValue : never;
+
+/**
+ * A - B.
+ *
+ * Note: Remove soon and use {@link Set.difference}! See:
+ * https://caniuse.com/mdn-javascript_builtins_set_difference
+ */
+export function difference<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): Set<T> {
+    const c = new Set(a);
+    for (const item of b) {
+        c.delete(item);
+    }
+    return c;
+}
